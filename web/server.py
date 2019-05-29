3339d1d50356db67148a3f6a5fbab7a851a94944
v1c7or20 <victor.pena@utec.edu.pe>
2019-05-29 12:22:14 -0500

    CRUD user implementation

--- web/server.py
+++ web/server.py
@@ -27,6 +27,45 @@ def get_users():
     return Response(json.dumps(data, cls=connector.AlchemyEncoder), mimetype='application/json')
 
 
+@app.route('/users', methods = ['POST'])
+def create_user():
+    c =  json.loads(request.form['values'])
+    user = entities.User(
+        username=c['username'],
+        name=c['name'],
+        fullname=c['fullname'],
+        password=c['password']
+    )
+    session = db.getSession(engine)
+    session.add(user)
+    session.commit()
+    return 'Created User'
+
+
+@app.route('/users', methods = ['PUT'])
+def update_user():
+    session = db.getSession(engine)
+    id = request.form['key']
+    user = session.query(entities.User).filter(entities.User.id == id).first()
+    c =  json.loads(request.form['values'])
+    for key in c.keys():
+        setattr(user, key, c[key])
+    session.add(user)
+    session.commit()
+    return 'Updated User'
+
+
+@app.route('/users', methods = ['DELETE'])
+def delete_message():
+    id = request.form['key']
+    session = db.getSession(engine)
+    messages = session.query(entities.User).filter(entities.User.id == id)
+    for message in messages:
+        session.delete(message)
+    session.commit()
+    return "Deleted Message"
+
+
 @app.route('/users/<id>', methods = ['GET'])
 def get_user(id):
     db_session = db.getSession(engine)
@@ -38,14 +77,8 @@ def get_user(id):
     message = { 'status': 404, 'message': 'Not Found'}
     return Response(message, status=404, mimetype='application/json')
 
-@app.route('/create_test_users', methods = ['GET'])
-def create_test_users():
-    db_session = db.getSession(engine)
-    user = entities.User(name="David", fullname="Lazo", password="1234", username="qwerty")
-    db_session.add(user)
-    db_session.commit()
-    return "Test user created!"
+
 
 if __name__ == '__main__':
     app.secret_key = ".."
-    app.run(port=8080, threaded=True, host=('127.0.0.1'))
+    app.run(debug=True, port=8080, threaded=True, host=('127.0.0.1'))
