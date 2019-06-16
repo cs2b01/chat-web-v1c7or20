cfb12a563df780c67452f473b2e5d6a0d9e2e673
v1c7or20 <victor.pena@utec.edu.pe>
2019-06-15 21:30:07 -0500

    html css

---
+++ web/templates/login.js
@@ -0,0 +1,33 @@
+function getData(){
+        $('#giphy').show();
+        $('#indice').hide();
+        $('#indice1').hide();
+        var username = $('#username').val();
+        var password = $('#password').val();
+        var message = JSON.stringify({
+                "username": username,
+                "password": password
+            });
+
+        $.ajax({
+            url:'/authenticate',
+            type:'POST',
+            contentType: 'application/json',
+            data : message,
+            dataType:'json',
+            success: function(response){
+                //alert(JSON.stringify(response));
+                $('#action').html(response['statusText']);
+            },
+            error: function(response){
+                //alert(JSON.stringify(response));
+                if(response['status']==401){
+                    $('#giphy').hide();
+                    $('#indice1').show();
+                }else{
+                    $('#giphy').hide();
+                    $('#indice').show();
+                }
+            }
+        });
+    }
